1a8eb04f03080bc7b1c26d595200721b332981d2
Aamir Tahir <saamirt@gmail.com>
2018-07-17 08:58:51 -0400

    added socket.io

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-var express = require('express');
-var app = express();
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function (req, res) {
-   res.sendFile(__dirname + '/index.html');
-   console.log(req.query.room);
+    res.sendFile(__dirname + '/index.html');
 });
 
- app.get('/files/:fileName', function (req, res) {
+app.get('/files/:fileName', function (req, res) {
     res.sendFile(__dirname + "\\" + req.params.fileName);
- });
+});
 
-var server = app.listen(8081, function () {
-   var port = server.address().port
-   
-   console.log("Secure Chat listening at http://localhost:" + port)
-})
+io.on('connection', function (socket) {
+    console.log('SOCKET.IO :: New User Joined');
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+        count--;
+    });
+});
+
+var server = http.listen(8082, function () {
+    var port = server.address().port
+
+    console.log('Secure Chat listening at http://localhost:' + port);
+});
